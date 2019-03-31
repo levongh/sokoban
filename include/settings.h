13ae738a1c1d1d26ea7be846704ffc6e47e9a300
levongh <levon_ghukasyan@live.com>
2019-03-31 09:59:57 +0400

    improved settings

--- include/settings.h
+++ include/settings.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <QString>
+#include <vector>
+
 class Settings
 {
 public:
@@ -10,8 +13,10 @@ public:
     void load();
     void save();
 
+    QString levelsPath()const;
     unsigned screenWidth() const;
     unsigned screenHeight() const;
+    const std::vector<std::pair<unsigned, bool> > getAvailableLevels() const;
 
 public:
     static Settings& getInstance();
@@ -23,5 +28,7 @@ private:
 private:
     unsigned m_scrrenWidth{0};
     unsigned m_scrrenHeight{0};
+
+    std::vector<std::pair<unsigned, bool> > m_availableLevels;
 };
 
--- src/settings.cpp
+++ src/settings.cpp
@@ -1,4 +1,12 @@
-#include "settings.h"
+#include <QDirIterator>
+
+#include "../include/settings.h"
+
+namespace  {
+
+static const QString levelPath="levels/";
+
+}
 
 Settings::Settings()
 {
@@ -13,11 +21,32 @@ Settings::~Settings()
 void Settings::load()
 {
     //TODO
+    m_availableLevels.clear();
+    QDirIterator iter(levelsPath());
+    while (iter.hasNext()) {
+        iter.next();
+        QFileInfo info(iter.fileInfo());
+        if (info.completeSuffix() == "lvl") {
+            m_availableLevels.push_back(std::make_pair<int, bool>(info.baseName().toInt(), true));
+        }
+    }
+    std::sort(m_availableLevels.begin(), m_availableLevels.end(),
+              [](const auto& lhs, const auto& rhs)->bool
+    {
+        return lhs.first < rhs.first;
+    });
+    //TODO do not allow gamer play all levels
 }
 
 void Settings::save()
 {
     //TODO
+}
+
+QString Settings::levelsPath()const
+{
+    //TODO make it changable
+    return levelPath;
 }
 
 unsigned Settings::screenWidth() const
@@ -38,4 +67,9 @@ Settings& Settings::getInstance()
 {
     static Settings instance;
     return instance;
+}
+
+const std::vector<std::pair<unsigned, bool> > Settings::getAvailableLevels() const
+{
+    return m_availableLevels;
 }
